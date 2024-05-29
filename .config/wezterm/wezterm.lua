fc368159e9ac6a390b9247c0f376943c990b8508
Phil Davies <7753727+bearmoth@users.noreply.github.com>
2024-05-30 09:42:10 +1000

    feat(wezterm): change colour scheme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,22 @@
 local wezterm = require("wezterm")
+
+-- The colour scheme name
+-- Second choice: "Afterglow",
+-- Third choice: "Catppuccin Mocha",
+local scheme = "Breeze (Gogh)"
+-- Obtain the definition of the color scheme
+local scheme_def = wezterm.color.get_builtin_schemes()[scheme]
+
 return {
 	font = wezterm.font("JetBrains Mono", { weight = "DemiBold" }),
-	color_scheme = "Catppuccin Mocha",
+	color_scheme = scheme,
+
+	colors = {
+		tab_bar = {
+			active_tab = {
+				bg_color = scheme_def.background,
+				fg_color = scheme_def.foreground,
+			},
+		},
+	},
 }
