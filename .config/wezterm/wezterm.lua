d304aeefdfa0fa228d42d9edc0d79fa0423f1ea4
Phil Davies <7753727+bearmoth@users.noreply.github.com>
2024-06-03 08:09:02 +1000

    feat(wezterm): configure multiplexing
    Configure pane splitting and navigation through WezTerm instead of tmux. Also configures navigation between Neovim panes and WezTerm panes.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,7 +7,52 @@ local scheme = "nord"
 -- Obtain the definition of the color scheme
 local scheme_def = wezterm.color.get_builtin_schemes()[scheme]
 
+local function is_vim(pane)
+	-- this is set by the plugin, and unset on ExitPre in Neovim
+	return pane:get_user_vars().IS_NVIM == "true"
+end
+
+-- Pane navigation key config
+local direction_keys = {
+	Left = "h",
+	Down = "j",
+	Up = "k",
+	Right = "l",
+	-- reverse lookup
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "META" or "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
+end
+
 return {
+	-- Leader key
+	--
+	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
+
+	-- Colour scheme
+	--
 	font = wezterm.font("JetBrains Mono", { weight = "DemiBold" }),
 	color_scheme = scheme,
 
@@ -20,5 +65,33 @@ return {
 		},
 	},
 
+	-- Tabs
 	hide_tab_bar_if_only_one_tab = true,
+
+	-- Key maps
+	keys = {
+		-- Panes
+		{
+			mods = "LEADER",
+			key = "-",
+			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			mods = "LEADER",
+			key = "|",
+			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		},
+
+		-- Pane navigation
+		split_nav("move", "h"),
+		split_nav("move", "j"),
+		split_nav("move", "k"),
+		split_nav("move", "l"),
+
+		-- Pane resize
+		split_nav("resize", "h"),
+		split_nav("resize", "j"),
+		split_nav("resize", "k"),
+		split_nav("resize", "l"),
+	},
 }
