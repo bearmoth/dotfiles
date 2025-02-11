0072b7603122c5cd6516e4eea0536d378984c196
Phil Davies <7753727+bearmoth@users.noreply.github.com>
2025-02-11 13:44:04 +1100

    feat(zsh): Add initial p10k theme
    Adds external resources: oh-my-zsh and powerlebel10k theme as well as
    initial zsh to load them.

---
+++ dot_config/zsh/oh-my-zsh.sh
@@ -0,0 +1,7 @@
+export ZSH="$HOME/.oh-my-zsh"
+DISABLE_AUTO_UPDATE="true"
+
+## Theme
+ZSH_THEME="powerlevel10k/powerlevel10k"
+source $ZSH/oh-my-zsh.sh
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
