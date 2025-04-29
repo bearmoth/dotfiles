c36167838569e7daa34fa1b57993e7099324a2f4
Phil Davies <7753727+bearmoth@users.noreply.github.com>
2025-04-29 21:25:01 +1000

    feat(nvim): Add justfile formatting

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "just",
+  callback = function()
+    vim.bo.expandtab = false
+    vim.bo.tabstop = 2
+    vim.bo.shiftwidth = 2
+  end,
+})
