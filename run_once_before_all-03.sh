08a6e799d44c92c459017f4df0e3775b7476529c
Phil Davies <7753727+bearmoth@users.noreply.github.com>
2025-07-04 17:00:57 +1000

    fix(nix): Check for existing instance before installing
    This prevents issues where an existing Nix instance creates issues for
    the installer.

--- run_once_before_all-03.sh
+++ run_once_before_all-03.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-echo "Installing Nix..."
-curl -fsSL https://install.determinate.systems/nix | sh -s -- install --determinate
+if ! command -v brew &>/dev/null; then
+  echo "Installing Nix..."
+  curl -fsSL https://install.determinate.systems/nix | sh -s -- install --determinate
+else
+  echo "Nix already installed"
+fi
