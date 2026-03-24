a65899990f20f0c5e0b004303ceee4b79afb256b
Phil Davies <7753727+bearmoth@users.noreply.github.com>
2026-03-24 12:30:05 +1100

    feat(dash): Install Dash gh extension

---
+++ dot_config/gh-dash/run_before_gh-dash.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if ! command -v gh &>/dev/null; then
+  exit 0
+fi
+
+# Exit early if extension is already installed
+if (gh extension list | grep dash) &>/dev/null; then
+  exit 0
+fi
+
+# Authenticate github
+# if ! (gh auth status | grep -q "Logged in to github.com"); then
+#   gh auth login
+# fi
+
+gh extension install dlvhdr/gh-dash
